f62afd65808c066752d806f6186c69662bad8da5
margaritasing <libiamfreitesm@gmail.com>
2022-05-14 15:39:16 -0300

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ListarProductosComponent},
+  {path: 'crear-producto', component: CrearProductoComponent},
+  {path: 'editar-producto/:id', component: CrearProductoComponent},
+  {path: '**', redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
