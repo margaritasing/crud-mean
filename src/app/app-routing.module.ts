8e8f42ea056a0aa1c544b9511e3780a8c6b52a61
margaritasing <libiamfreitesm@gmail.com>
2022-05-14 16:10:26 -0300

    agrego estilos boostrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   {path: '', component: ListarProductosComponent},
   {path: 'crear-producto', component: CrearProductoComponent},
   {path: 'editar-producto/:id', component: CrearProductoComponent},
-  {path: '**', redirectTo:''}
+  {path: '**', redirectTo:'', pathMatch: 'full'}
 ];
 
 @NgModule({
