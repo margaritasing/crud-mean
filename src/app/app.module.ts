d407a7216350093c3b259ba331ca575dea2ba540
margaritasing <libiamfreitesm@gmail.com>
2022-05-14 17:28:34 -0300

    agrego toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +18,9 @@ import { ListarProductosComponent } from './components/listar-productos/listar-p
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Producto } from 'src/app/models/producto';
 
 @Component({
   selector: 'app-crear-producto',
@@ -11,7 +13,7 @@ export class CrearProductoComponent implements OnInit {
   productoForm: FormGroup;
 
 //inyectando el formulario reactivo
-  constructor(private fb: FormBuilder) { 
+  constructor(private fb: FormBuilder, private router:Router) { 
     this.productoForm = this.fb.group({
       producto:['', Validators.required],
       categoria:['', Validators.required],
@@ -23,8 +25,14 @@ export class CrearProductoComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  agregarproductos(){
-    console.log(this.productoForm)
+  agregarProductos(){   
+    const PRODUCTO: Producto = {
+      nombre:this.productoForm.get('producto')?.value,
+      categoria:this.productoForm.get('categoria')?.value,
+      ubicacion:this.productoForm.get('ubicacion')?.value,
+      precio:this.productoForm.get('precio')?.value,
+    }
+    this.router.navigate(['/'])    
   }
 
 }
